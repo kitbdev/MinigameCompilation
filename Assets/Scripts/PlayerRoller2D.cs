9a671068651f690f3c894d4de1929eb9eff7d15e
ianb96 <ianb96@gmail.com>
2017-02-20 08:41:08 -0500

    mobile settings

---
+++ Assets/Scripts/PlayerRoller2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoller2D : MonoBehaviour {
+
+
+
+	Rigidbody rb;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		rb.AddForce(Input.gyro.gravity, ForceMode.Acceleration);
+		if(Input.touchCount==1) {
+			Instantiate(gameObject, Camera.main.ScreenToWorldPoint(Input.touches[0].position), Quaternion.identity);
+		}
+	}
+}
