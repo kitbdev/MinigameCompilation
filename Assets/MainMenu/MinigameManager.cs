3bd26b29a04d94fcd056c98ec51c28cff1ed4845
ianb96 <ianb96@gmail.com>
2017-04-17 12:38:32 -0400

    menu screen fixes

--- Assets/MainMenu/MinigameManager.cs
+++ Assets/MainMenu/MinigameManager.cs
@@ -9,11 +9,23 @@ public class MinigameManager : MonoBehaviour {
 	public CanvasGroup endGameScreen;
 	public CanvasGroup levelSelectScreen;
 	public CanvasGroup mainScreen;
+	public Text scoreText;
 	public int curGameIndex {get; private set;}
 	public int totalScore;
+	LeaderboardManager lm;
 
 	void Start () {
+		lm = GetComponent<LeaderboardManager>();
 		DontDestroyOnLoad(gameObject);
+		
+		// reset screens
+		mainScreen.GetComponent<RectTransform>().offsetMin = new Vector2(0,0);
+		mainScreen.GetComponent<RectTransform>().offsetMax = new Vector2(1,1);
+		levelSelectScreen.GetComponent<RectTransform>().offsetMin = new Vector2(0,0);
+		levelSelectScreen.GetComponent<RectTransform>().offsetMax = new Vector2(1,1);
+		endGameScreen.GetComponent<RectTransform>().offsetMin = new Vector2(0,0);
+		endGameScreen.GetComponent<RectTransform>().offsetMax = new Vector2(1,1);
+		
 		ChangeScreen(mainScreen, true);
 		ChangeScreen(levelSelectScreen, false);
 		ChangeScreen(endGameScreen, false);
@@ -23,7 +35,7 @@ public class MinigameManager : MonoBehaviour {
 	}
 	/// load the specified minigame scene and start playing
 	public void StartMinigame(int index) {
-		if (index < SceneManager.sceneCountInBuildSettings)
+		if (index >= SceneManager.sceneCountInBuildSettings)
 		{
 			Debug.LogWarning("Not enough scenes in build settings!");
 			return;
@@ -35,8 +47,9 @@ public class MinigameManager : MonoBehaviour {
 	/// Shows the end game screen and displays the final score to the user
 	/// Allows the user to go back to the main menu
 	public void FinishMinigame(int score=0) {
+		scoreText.text = "Score: "+score;
 		ChangeScreen(endGameScreen, true);
-		// do something with score
+		// TODO: submit score if logged in
 		
 	}
 	/// Return to the main menu
@@ -47,14 +60,22 @@ public class MinigameManager : MonoBehaviour {
 		SceneManager.LoadScene(0);
 		ChangeScreen(mainScreen, true);
 	}
+	/// Return to the main menu
+	/// Called by the return button
+	public void BackToMenuScreen() {
+		ChangeScreen(levelSelectScreen, false);
+		curGameIndex = 0;
+		ChangeScreen(mainScreen, true);
+	}
 	/// Exits the application immediately
 	public void ExitApp() {
 		Application.Quit();
 	}
 	private void ChangeScreen(CanvasGroup cg, bool turnOn) {
-		cg.alpha = turnOn ? 1 : 0;
+		cg.alpha = turnOn ? 1f : 0f;
 		cg.interactable = turnOn;
 		cg.blocksRaycasts = turnOn;
+		Time.timeScale = turnOn ? 0f : 1f;
 	}
 	/// Called by the main menu screen's button to go to the level selection screen
 	public void ShowLevelSelection() {
