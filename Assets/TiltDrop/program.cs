869af33aae2c9c1cdf90394365f6fb153dd58ba1
ianb96 <ianb96@gmail.com>
2017-04-20 14:19:06 -0400

    incorporate tiledrop minigame

---
+++ Assets/TiltDrop/program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class program : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
+		rb.AddForce(movement * speed * Time.deltaTime);
+	}
+}
